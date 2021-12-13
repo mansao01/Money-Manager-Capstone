83f4bf62ad3a730d7b51e00a612ba5f2f941ddb6
Novri Lukman Zyarif <novrilukman15@gmail.com>
2021-12-13 19:51:07 +0700

    make a new Activity,  AddTransaction.kt and activity_add_transaction.xml

---
+++ app/src/main/java/com/mansao/moneymanagercapstone/ui/addtransacation/AddTransaction.kt
@@ -0,0 +1,12 @@
+package com.mansao.moneymanagercapstone.ui.addtransacation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mansao.moneymanagercapstone.R
+
+class AddTransaction : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_transaction)
+    }
+}
