6041a03176c0c52e716009972d942a0c8ea131bc
Novri Lukman Zyarif <novrilukman15@gmail.com>
2021-12-14 18:53:14 +0700

    make ui activity_add_transaction.xml

--- app/src/main/java/com/mansao/moneymanagercapstone/ui/addtransacation/AddTransaction.kt
+++ app/src/main/java/com/mansao/moneymanagercapstone/ui/addtransacation/AddTransaction.kt
@@ -2,11 +2,14 @@ package com.mansao.moneymanagercapstone.ui.addtransacation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mansao.moneymanagercapstone.R
+import com.mansao.moneymanagercapstone.databinding.ActivityAddTransactionBinding
 
 class AddTransaction : AppCompatActivity() {
+    private lateinit var binding : ActivityAddTransactionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_transaction)
+        binding = ActivityAddTransactionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
