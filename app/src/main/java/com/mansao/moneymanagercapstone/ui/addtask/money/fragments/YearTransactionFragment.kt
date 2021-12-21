8ac5eb87005381479aa6e5caf36381c709cf35ed
Novri Lukman Zyarif <novrilukman15@gmail.com>
2021-12-21 14:53:39 +0700

    make ui better

--- app/src/main/java/com/mansao/moneymanagercapstone/ui/addtask/money/fragments/YearTransactionFragment.kt
+++ app/src/main/java/com/mansao/moneymanagercapstone/ui/addtask/money/fragments/YearTransactionFragment.kt
@@ -1,15 +1,14 @@
 package com.mansao.moneymanagercapstone.ui.addtask.money.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mansao.capstonedraft.helper.ViewModelFactory
-import com.mansao.moneymanagercapstone.R
 import com.mansao.moneymanagercapstone.databinding.FragmentYearTransactionBinding
 import com.mansao.moneymanagercapstone.helper.DateHelper
 import com.mansao.moneymanagercapstone.ui.addtask.money.MoneyAddUpdateActivity
